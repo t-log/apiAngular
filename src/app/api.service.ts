c6eb4c0845e7bd68fa4d437db742ca5fcd1240a9
akhil <akhil.paul@ymail.com>
2022-11-29 17:16:52 +0530

    Adds fetch from url for productsView

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,4 +11,5 @@ export class ApiService {
   fetchQuotes=()=>{return this.http.get("https://dummyjson.com/quotes")}
   fetchPassenger=()=>{return this.http.get("https://api.instantwebtools.net/v1/passenger?page=0&size=30")}
   fetchPets=()=>{return this.http.get("https://api.publicapis.org/entries")}
+  fetchProducts=()=>{return this.http.get("https://dummyjson.com/products")}
 }
